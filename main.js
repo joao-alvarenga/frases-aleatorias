ea52622b12da065a974caef0f1f87a9f2f2f414a
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-04 17:25:53 -0300

    add updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // variaveis para as cores 
 var containerCor = [
-    '#60ee70', '#505050','#458787','#963489','#121212','#797988','#149070','#05789d'
+    '#60ee70','#505050','#458787','#963489','#121212','#797988','#149070','#05789d'
 ];
 
 var bodyCor = [
@@ -18,7 +18,7 @@ function click()
     /* checar se a cor atual é o ultimo obcjeto no array, 
     se for colocar a cor de volta a 0 (a primeira cor no array) se não for 
     acrescentamos 1 a cor atual. */
-    if (currentColor == colors.length-1) currentColor = 0;
+    if (currentColor == bodyCor.length-1) currentColor = 0;
     else currentColor++; 
 
     /* agora podemos acrescentar as cores ao body e ao container */
